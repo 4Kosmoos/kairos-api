2f81e9a9f9271b49bd981522c014242322c7adc9
4Kosmoos <firmin.shu@gmail.com>
2025-05-14 11:45:59 +0200

    first commit

---
+++ src/test/java/dev/kosmoos/kairosapi/KairosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kosmoos.kairosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KairosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
